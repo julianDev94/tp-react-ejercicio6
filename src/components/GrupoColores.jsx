70c6de002ceb6621b92cbed25f2f7b05376140c5
JulianAballay <juliancabj1210@gmail.com>
2024-02-02 13:44:08 -0300

    creo componente GrupoColores

---
+++ src/components/GrupoColores.jsx
@@ -0,0 +1,12 @@
+import { CardGroup } from "react-bootstrap"
+
+
+const GrupoColores = () => {
+    return (
+        <CardGroup>
+            
+        </CardGroup>
+    );
+};
+
+export default GrupoColores;
